484ffc1952f2c9324aa02f3e6c737a720d9ce2cd
Jennifer Gentry <jennifer@gentrytechnology.com>
2025-06-04 14:21:26 -0400

    Create James.py

---
+++ 1.3 PA - Printing the Corporate Header/James.py
@@ -0,0 +1,13 @@
+def printheader():
+  print()
+  print('Name: James \nDepartment: IT \nComputer: Apple Air')
+  print('Date and Time: 6/22/2022 10:30pm\nName of Report: A Great Report')
+
+def printfooter():
+  print()
+
+printheader()
+print ('\nReport goes here\n')
+printfooter()
+
+print('(c) www.jamwit3278.com     A Great Report     Page 1 of 1')
