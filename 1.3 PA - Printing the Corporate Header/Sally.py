9543a4faecee02e246f98abc4977f52b9a568d52
Jennifer Gentry <jennifer@gentrytechnology.com>
2025-06-04 14:09:34 -0400

    Create Sally.py

---
+++ 1.3 PA - Printing the Corporate Header/Sally.py
@@ -0,0 +1,19 @@
+def printheader():
+  Name = 'Sally'
+  Department = 'IT"
+  Computer = "HP 3000P'
+  DateAndTime = ' "6/19/2022 11:30am' "
+  NameOfReport = 'A Great Report"
+
+  print('Name: "+Nam)
+  print("Department: '+ Dept)
+  print(' "Computer: '"+Comp)
+  print('Date and Time: " +DandT)
+  print("Name of Report: '+ ReportName)
+
+def printfooter():
+  print(' (c) www.salbre1189.com     " + A Great Report     + "Page 1 of 1')
+
+printheader(ReportName)
+print("\nReport goes here\n')
+printfooter()
