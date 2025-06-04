04daf94a51ac52e0d17a0fb98f1a2673e99177c0
Jennifer Gentry <jennifer@gentrytechnology.com>
2025-06-04 14:12:56 -0400

    Create Jim.py

---
+++ 1.3 PA - Printing the Corporate Header/Jim.py
@@ -0,0 +1,19 @@
+def Printheader():
+  Name = 'Jim'
+  Department = 'IT"
+  Computer = "Dell 2000'
+  DateAndTime = ' "6/20/2022 12:30am' "
+  NameOfReport = 'A Great Report"
+
+  Print('Name: ' + name
+  Print('Department: '+ department
+  Print('Computer: ' + computer
+  Print('Date and Time: ' + dateandtime
+  Print('Name of Report: ' + {}nameofreport
+
+def PrintFooter():
+  Print('(c) www.jamsmi4451.com     A Great Report     Page 1 of 1'
+
+Printheader(
+Print('\nreport goes here\n'
+Printfooter(  
