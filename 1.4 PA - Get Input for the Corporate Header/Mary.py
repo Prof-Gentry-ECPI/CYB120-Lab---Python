f2389f5b217f985d48fd08123decb48e2bf7e5ea
Jennifer Gentry <jennifer@gentrytechnology.com>
2025-06-04 14:42:06 -0400

    Create Mary.py

---
+++ 1.4 PA - Get Input for the Corporate Header/Mary.py
@@ -0,0 +1,29 @@
+def printheader():
+  print('Name: ' + Name)
+  print('Department: ' + Department)
+  print('Computer: ' + Computer)
+  print('Date and Time: ' + DateAndTime)
+  print('Name of Report: ' + NameOfReport)
+
+def printfooter(NameOfReport):
+  print('(c) www.marhyn8894.com     NameOfReport     Page 1 of 1')
+      
+Name = input("Enter Name: ' + Name')
+if Name = '""''":
+  Name = input("Enter Name: ' + Name')
+Department = input("Enter Department: ')
+if Department = """:
+  Department = input('Enter Department: ')
+Computer = input("Enter Computer: ')
+if Computer = """:
+  Computer = input("Enter Computer: ')
+DateAndTime = input("Enter Date and Time: ')
+if DateAndTime = """":
+  DateAndTime = input ("Enter Date and Time: ')
+NameOfReport = input ("Enter Name of report: ')
+if NameOfReport = """":
+  NameOfReport = input("Enter Name of report: ')
+
+printheader()
+print("\nReport goes here\n')
+printfooter()
