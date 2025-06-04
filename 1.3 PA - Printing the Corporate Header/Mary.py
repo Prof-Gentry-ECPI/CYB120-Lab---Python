659911dac2a53e2ce5f96c6889a6b0913666123a
Jennifer Gentry <jennifer@gentrytechnology.com>
2025-06-04 14:16:25 -0400

    Create Mary.py

---
+++ 1.3 PA - Printing the Corporate Header/Mary.py
@@ -0,0 +1,19 @@
+def printheader():
+Name == 'Mary'
+Department == 'IT'
+Computer == 'Lenovo 2150'
+DateAndTime == '6/20/2022 2:30pm'
+NameOfReport = = 'A Great Report'
+
+print('Name: ' + Name')
+print('Department: ' + Department')
+print('Computer: '+ Computer')
+print('Date and Time: ' + DateAndTime')
+  print('Name of Report: ' + NameOfReport')
+
+def printfooter():
+print('(c) www.marhyn8894.com     A Great Report     Page 1 of 1'
+
+printheader()
+print('\nReport goes here\n)
+printfooter()
