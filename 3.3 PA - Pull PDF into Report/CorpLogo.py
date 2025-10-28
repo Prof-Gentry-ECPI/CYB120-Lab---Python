b29d39528041b1a9fe85353c25e95c9bcb35e24e
Jennifer Gentry <jennifer@gentrytechnology.com>
2025-10-28 07:35:43 -0400

    Add functions to print report header and footer

---
+++ 3.3 PA - Pull PDF into Report/CorpLogo.py
@@ -0,0 +1,9 @@
+def printHeader(info):
+  print ("Name: ", info[0])
+  print ("Department: ", info[1])
+  print ("Computer Name: ", info[2])
+  print ("Date: ", info[3])
+  print ("Report: ", info[4])
+
+def printFooter(id, reportName):
+  print(f"www.{id}.com\t{reportName}\tPage 1 of 1")
